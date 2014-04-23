6f3b3a132993ec4648a8b63e1588210426774ea4
Ricardo Mendoza Huesca <ingsisrmh@gmail.com>
2014-04-22 23:44:51 -0500

    Create querysel.js

---
+++ querysel.js
@@ -0,0 +1,17 @@
+<!DOCYPE html>
+
+<html lang="es">
+<head>
+	<meta charset="utf-8"/>
+	<title>Incrustado</title>
+	<link rel="stylesheet" href="estilos.css"/>
+	<script src="querysel.js"></script>
+</head>
+
+<body>
+	<section id="principal">
+		<p id="presionar">Pulsa aquí</p>
+		<p>Aquí no puedes pulsar</p>
+	</section>
+</body>
+</html>
